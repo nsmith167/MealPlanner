2c71bc67a4c556cf8846bde4b72288b6f6a2b5c1
nsmith167 <n.smith167@gmail.com>
2020-08-07 09:00:35 -0400

    Adding healthcheck. Kinda.

---
+++ src/main/java/com/smithfam/mealplanner/controller/StatusEndpoint.java
@@ -0,0 +1,16 @@
+package com.smithfam.mealplanner.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusEndpoint {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@ResponseBody
+	public String getRecipes() {
+		return "I am running, don't worry";
+	}
+}
