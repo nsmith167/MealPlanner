15632158ccfc484605b9f8d92c4518b9d198229b
nsmith167 <n.smith167@gmail.com>
2020-08-07 11:05:05 -0400

    Cleaning up configuration for redis more

--- src/main/java/com/smithfam/mealplanner/config/RedisConfig.java
+++ src/main/java/com/smithfam/mealplanner/config/RedisConfig.java
@@ -1,10 +1,13 @@
 package com.smithfam.mealplanner.config;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.time.Duration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisClientConfiguration.JedisClientConfigurationBuilder;
@@ -13,14 +16,23 @@ import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
+	
+	private Logger logger = LoggerFactory.getLogger(RedisConfig.class);
 
 	@Bean
 	JedisConnectionFactory jedisConnectionFactory() {
 		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setHostName(System.getenv("REDISCLOUD_URL"));
-        redisStandaloneConfiguration.setDatabase(0);
-
-        JedisClientConfigurationBuilder jedisClientConfiguration = JedisClientConfiguration.builder();
+		URI redisURI;
+		try {
+			redisURI = new URI(System.getenv("REDISCLOUD_URL"));
+			redisStandaloneConfiguration.setHostName(redisURI.getHost());
+			redisStandaloneConfiguration.setPort(redisURI.getPort());
+	        redisStandaloneConfiguration.setDatabase(0);
+		} catch (URISyntaxException e) {
+			logger.error(e.getMessage());
+		}
+		
+		JedisClientConfigurationBuilder jedisClientConfiguration = JedisClientConfiguration.builder();
         jedisClientConfiguration.connectTimeout(Duration.ofSeconds(60));
 
         JedisConnectionFactory jedisConFactory = new JedisConnectionFactory(redisStandaloneConfiguration, jedisClientConfiguration.build());
